0c65226c8d9c77740fc9c9383af7437206718184
kanemoto-r <10150406reika@gmail.com>
2022-08-08 10:26:20 +0900

    完成ファイルを追加

---
+++ js/humburger.js
@@ -0,0 +1,18 @@
+$(function () {
+
+
+    // 
+    // ハンバーガーメニューのボタンの変化とクリックしたらナビゲーションが出現
+    // 
+    $(".openbtn").click(function () {//ボタンがクリックされたら
+        $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+        $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    });
+
+    $("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+        $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+        $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+    });
+
+
+});
